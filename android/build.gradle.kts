db65f439d5bd3a81691567795d646f8b8b8ca922
liang-work <tianhesl@outlook.com>
2026-07-12 14:03:27 +0800

    :robot: fixed a issue about build failed again 4

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,18 +16,10 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-subprojects {
-    plugins.withId("com.android.application") {
-        extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
-            compileSdk = 36
-        }
-    }
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.api.dsl.LibraryExtension> {
-            compileSdk = 36
+    afterEvaluate {
+        val ext = extensions.findByName("android")
+        if (ext is com.android.build.api.dsl.CommonExtension) {
+            ext.compileSdk = 36
         }
     }
 }
